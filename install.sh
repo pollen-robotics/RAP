bde1ddc1e90af33a25093fd0bc5a51fcdddb060e
Pierre Rouanet <pierre.rouanet@gmail.com>
2020-03-13 11:10:02 +0100

    Move config script in separate folder.

--- install.sh
+++ install.sh
@@ -11,20 +11,20 @@ apt install -y dnsmasq hostapd
 if [ -f /etc/hostapd/hostapd.conf ]; then
   mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.old
 fi
-cp etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf
+cp scripts/etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf
 
 if [ -f /etc/default/hostapd ]; then
   mv /etc/default/hostapd /etc/default/hostapd.old
 fi
-cp etc/default/hostapd /etc/default/hostapd
+cp scripts/etc/default/hostapd /etc/default/hostapd
 
 if [ -f /etc/dnsmasq.conf ]; then
   mv /etc/dnsmasq.conf /etc/dnsmasq.conf.old
 fi
-cp etc/dnsmasq.conf /etc/dnsmasq.conf
+cp scripts/etc/dnsmasq.conf /etc/dnsmasq.conf
 
-cp etc/systemd/system/reachy-access-point.service /etc/systemd/system/reachy-access-point.service
-cp usr/bin/reachy-access-point.sh /usr/bin/reachy-access-point
+cp scripts/etc/systemd/system/reachy-access-point.service /etc/systemd/system/reachy-access-point.service
+cp scripts/usr/bin/reachy-access-point.sh /usr/bin/reachy-access-point
 chmod +x /usr/bin/reachy-access-point
 
 systemctl enable reachy-access-point.service
